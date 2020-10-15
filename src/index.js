d90141b3e5fb6a78d7c600ce021ac675fce692b8
Grace Chen <gychen2@illinois.edu>
2020-10-15 13:34:20 -0700

    Logout code added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
-// import { Auth } from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
+import config from 'cofig.js';
 // import css
 
 // amplify auth -> cognito
@@ -12,12 +13,12 @@ function LoginInfo() {
     async function handleSubmit(event) {
       event.preventDefault();
       
-      // try {
-      //     await Auth.signin(this.state.email, this.state.password);
-      //     alert("Logged in");
-      // } catch(e) {
-      //     alert(e.message);
-      // }
+      try {
+          await Auth.signin(email, password);
+          alert("Logged in");
+      } catch(e) {
+          alert(e.message);
+      }
     }
 
     return (
@@ -42,5 +43,28 @@ function LoginInfo() {
         </div>
     );
 }
+
+// async function signOut() {
+//   try {
+//     await Auth.signOut();
+//   } catch (e) {
+//     console.log(e.message);
+//   }
+// }
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID
+  }
+});
 
 ReactDOM.render(<LoginInfo />, document.getElementById('root'))
