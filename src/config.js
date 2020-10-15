c9ebdaaecb5d5afaee2dba9118f9d71ab56f5401
Grace Chen <gychen2@illinois.edu>
2020-10-15 13:35:19 -0700

    Details for S3 and Cognito. S3 bucket not yet created

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+    s3: {
+      REGION: "us-west-2",
+      BUCKET: "validator-bucket" // CREATE BUCKET... DOES NOT EXIST YET
+    },
+    cognito: {
+      REGION: "us-west-2",
+      USER_POOL_ID: "us-west-2_JFo5xm09s",
+      APP_CLIENT_ID: "Validator",
+      IDENTITY_POOL_ID: "us-west-2:f923ab14-a6aa-4792-8a21-71b5532896f1"
+    }
+  };
