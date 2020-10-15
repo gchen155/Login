2a41bdd14ee78a87fa5e76c9df96c0aa28ed548e
Grace Chen <gychen2@illinois.edu>
2020-10-14 21:52:01 -0700

    Committing src folder

--- src/index.js
+++ src/index.js
@@ -1,17 +1,104 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import { Auth } from 'aws-amplify';
+// import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
+// import css
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// amplify auth -> cognito
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// class LoginInfo extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             email: '',
+//             password: ''
+//         }
+//     }
+
+//     async handleSubmit(event) {
+//         event.preventDefault();
+        
+//         try {
+//             await Auth.signin(this.state.email, this.state.password);
+//             alert("Logged in");
+//         } catch(e) {
+//             alert(e.message);
+//         }
+//     }
+//     // <AmplifyAuthenticator usernameAlias="email" />
+//     render() {
+//         return (
+//             <form id="Login" onSubmit={this.handleSubmit.bind(this)}>
+//                 <FormGroup controlId="email">
+//                     <ControlLabel>IAM Email</ControlLabel>
+//                 </FormGroup>
+//                 <FormControl
+//                     type="email" 
+//                     value={this.state.email}
+//                     onChange={this.onEmailInsert.bind(this)} 
+//                 /><br /><br />
+//                 <FormGroup controlId="password">
+//                     <ControlLabel>Password</ControlLabel>
+//                 </FormGroup>
+//                 <FormControl 
+//                     type="password" 
+//                     value={this.state.password}
+//                     onChange={this.onPassInsert.bind(this)}
+//                 /><br /><br />
+//                 <h1>{this.state.email}</h1>
+//                 <Button appearance="primary" type="submit">Login</Button>
+//             </form>
+//         )
+//     }
+
+//     onEmailInsert(event) {
+//         this.setState({email: event.target.value})
+//     }
+
+//     onPassInsert(event) {
+//         this.setState({password: event.target.value})
+//     }
+// }
+
+// // onClick submit, the inputs go to amplify/dynamoDB system
+
+function LoginInfo() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function handleSubmit(event) {
+      event.preventDefault();
+      
+      try {
+          await Auth.signin(this.state.email, this.state.password);
+          alert("Logged in");
+      } catch(e) {
+          alert(e.message);
+      }
+    }
+
+    return (
+        <div className="Login">
+            <form onSubmit={handleSubmit}>
+                <FormGroup controlId="email" bsSize="large">
+                    <ControlLabel>Email</ControlLabel><br />
+                    <FormControl 
+                    type="email" 
+                    value={email}
+                    onChange={e => setEmail(e.target.value)} />
+                </FormGroup><br />
+                <FormGroup controlId="password" bsSize="large">
+                    <ControlLabel>Password</ControlLabel><br />
+                    <FormControl 
+                    type="password" 
+                    value={password}
+                    onChange={e => setPassword(e.target.value)} />
+                </FormGroup><br />
+                <Button appearance="primary" type="submit">Login</Button>
+            </form>
+        </div>
+    );
+}
+
+ReactDOM.render(<LoginInfo />, document.getElementById('root'))
