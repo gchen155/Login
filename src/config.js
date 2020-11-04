0d571d9953d1eb5e13c1b70840c7bc5d09ddf617
Grace Chen <gychen2@illinois.edu>
2020-11-03 17:19:23 -0800

    update

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 export default {
-    s3: {
-      REGION: "us-west-2",
-      BUCKET: "validator-bucket" // CREATE BUCKET... DOES NOT EXIST YET
-    },
-    cognito: {
-      REGION: "us-west-2",
-      USER_POOL_ID: "us-west-2_JFo5xm09s",
-      APP_CLIENT_ID: "Validator",
-      IDENTITY_POOL_ID: "us-west-2:f923ab14-a6aa-4792-8a21-71b5532896f1"
-    }
-  };
+  s3: {
+    REGION: "us-west-2",
+    BUCKET: "validator-bucket-amplify"
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_JFo5xm09s",
+    APP_CLIENT_ID: "t8gid35b45bgnfduu7jqpedu3",
+    IDENTITY_POOL_ID: "us-west-2:f923ab14-a6aa-4792-8a21-71b5532896f1"
+  }
+};
