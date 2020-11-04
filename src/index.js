df624b966033d097ced382a3e03a40fc67e6845a
Grace Chen <gychen2@illinois.edu>
2020-11-03 17:19:04 -0800

    updated

--- src/index.js
+++ src/index.js
@@ -1,35 +1,68 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 import { Amplify, Auth } from 'aws-amplify';
-import config from 'cofig.js';
-// import css
+import { withAuthenticator } from '@aws-amplify/ui-react'; //AmplifySignOut
+//import awsconfig from './aws-exports';
+
+import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import config from './config';
+import './index.css';
 
 // amplify auth -> cognito
+Amplify.configure(config);
+// {
+//     Auth: {
+//       mandatorySignIn: true,
+//       region: config.cognito.REGION,
+//       userPoolId: config.cognito.USER_POOL_ID,
+//       identityPoolId: config.cognito.IDENTITY_POOL_ID,
+//       userPoolWebClientId: config.cognito.APP_CLIENT_ID
+//     },
+//     Storage: {
+//       region: config.s3.REGION,
+//       bucket: config.s3.BUCKET,
+//       identityPoolId: config.cognito.IDENTITY_POOL_ID
+//     }
+//   }
+
+const App = () => (
+    <div>
+      <LoginInfo />
+      Log in
+    </div>
+  );
+  
+export default withAuthenticator(App);
+
 function LoginInfo() {
-    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+
+    // Auth.signUp({
+    //     username: 'gchen19@gmail.com',
+    //     password: 'Heyman123!',
+    //     attributes: {
+    //         email: 'gchen19@gmail.com',
+    //     },
+    // });
 
     async function handleSubmit(event) {
       event.preventDefault();
       
-      try {
-          await Auth.signin(email, password);
-          alert("Logged in");
-      } catch(e) {
-          alert(e.message);
-      }
+        Auth.signin(username, password)
+            .then(success => console.log("Logged in"))
+            .catch(err => console.log(err+"bad"));
     }
 
     return (
         <div className="Login">
             <form onSubmit={handleSubmit}>
-                <FormGroup controlId="email" bsSize="large">
-                    <ControlLabel>Email</ControlLabel><br />
+                <FormGroup controlId="user" bsSize="large">
+                    <ControlLabel>Username</ControlLabel><br />
                     <FormControl 
-                    type="email" 
-                    value={email}
-                    onChange={e => setEmail(e.target.value)} />
+                    type="username"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)} />
                 </FormGroup><br />
                 <FormGroup controlId="password" bsSize="large">
                     <ControlLabel>Password</ControlLabel><br />
@@ -38,33 +71,20 @@ function LoginInfo() {
                     value={password}
                     onChange={e => setPassword(e.target.value)} />
                 </FormGroup><br />
-                <Button appearance="primary" type="submit">Login</Button>
+                <Button appearance="primary" type="submit">Login</Button><br />
+                <Button appearance="primary" onClick={signOut}>Sign Out</Button><br />
             </form>
         </div>
     );
+    // <Button appearance="primary" onClick=>Sign Up</Button></Button>
+
+    async function signOut() {
+        try {
+            await Auth.signOut();
+        } catch (e) {
+            console.log("error signing out: ", e.message);
+        }
+    }
 }
-
-// async function signOut() {
-//   try {
-//     await Auth.signOut();
-//   } catch (e) {
-//     console.log(e.message);
-//   }
-// }
-
-Amplify.configure({
-  Auth: {
-    mandatorySignIn: true,
-    region: config.cognito.REGION,
-    userPoolId: config.cognito.USER_POOL_ID,
-    identityPoolId: config.cognito.IDENTITY_POOL_ID,
-    userPoolWebClientId: config.cognito.APP_CLIENT_ID
-  },
-  Storage: {
-    region: config.s3.REGION,
-    bucket: config.s3.BUCKET,
-    identityPoolId: config.cognito.IDENTITY_POOL_ID
-  }
-});
 
 ReactDOM.render(<LoginInfo />, document.getElementById('root'))
